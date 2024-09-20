7fdc04ef82ad1512908b560b781fa0b4866365c2
moises-course <112935181+moises-course@users.noreply.github.com>
2024-09-20 11:12:19 +0200

    Create xmlrpc.php

---
+++ xmlrpc.php
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+  <service>
+    <engineName>VoltAI Innovations</engineName>
+    <engineLink>https://www.voltaiinnovations.com</engineLink>
+    <homePageLink>https://www.voltaiinnovations.com</homePageLink>
+    <apis>
+      <api name="VoltAI" blogID="1" preferred="true" apiLink="https://www.voltaiinnovations.com/xmlrpc.php" />
+      <api name="Movable Type" blogID="1" preferred="false" apiLink="https://www.voltaiinnovations.com/xmlrpc.php" />
+      <api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://www.voltaiinnovations.com/xmlrpc.php" />
+      <api name="Blogger" blogID="1" preferred="false" apiLink="https://www.voltaiinnovations.com/xmlrpc.php" />
+    </apis>
+  </service>
+</rsd>
